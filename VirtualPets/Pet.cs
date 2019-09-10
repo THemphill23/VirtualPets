a09f30d0cc08a1735712a39c53cc4a29b449fa6e
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-10 15:08:30 -0400

    Feeding and Stats
    cd

--- VirtualPets/Pet.cs
+++ VirtualPets/Pet.cs
@@ -9,15 +9,29 @@ namespace VirtualPets
         public string PetName { get; set; }
         public enum Type { organic, robotic }
         public string PetType { get; set; }
+        public string PetSpecies { get; set; }
+
+        public int PetHunger = 10;
+        public int PetBoredom = 10;
+        public int PetHealth = 10;
 
         public void CreatePet(string petName)
         {
-            //var pet = new Pet();
-
             this.PetName = petName;
-
-            
         }
+
+        //public void CreateHunger(int petHunger)
+        //{
+        //    this.PetHunger = petHunger;
+        //}
+        //public void CreateBoredom(int petBoredom)
+        //{
+        //    this.PetBoredom = petBoredom;
+        //}
+        //public void CreateHealth(int petHealth)
+        //{
+        //    this.PetHealth = petHealth;
+        //}
     }
 
 
--- VirtualPets/PetAmok.cs
+++ VirtualPets/PetAmok.cs
@@ -17,9 +17,10 @@ namespace VirtualPets
                 Console.WriteLine();
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("Enter 'L' to leave");
-                Console.WriteLine("Type 'Add' to add a pet");
-                Console.WriteLine("After you Add a pet, type 'Info' to view its information");
-
+                Console.WriteLine("Type 'Add' to add a pet to shelter");
+                Console.WriteLine("After you Add a pet, type 'Info' to view its information.");
+                Console.WriteLine("Type 'Info' to view its information, 'Status' to view its status,");
+                Console.WriteLine("After you Add a pet, type 'Feed' to give your pet food.");
                 string input = Console.ReadLine().ToLower();
 
                 switch (input)
@@ -31,23 +32,76 @@ namespace VirtualPets
                     case ("add"):
                         Console.WriteLine("What is your pet's name?");
                         string petName = Console.ReadLine();
+                        //int petHunger = Convert.ToInt32("10");
+                        //int petBoredom = Convert.ToInt32("10");
+                        //int petHealth = Convert.ToInt32("10");
                         Console.Clear();
                         myPet.CreatePet(petName);
 
                         Console.WriteLine("Your pet's name is " + petName);
-                        Console.WriteLine("Is your pet organic or robotic?");
-                        string petType = Console.ReadLine().ToLower();
-                        Console.Clear();
-                        myPet.PetType = petType;
+                        while (true)
+                        {
+                            Console.WriteLine("Is your pet organic or robotic?");
+                            string petType = Console.ReadLine().ToLower();
+                            myPet.PetType = petType;
+
+                            if (petType == "organic")
+                                break;
+                            else if (petType == "robotic")
+                                break;
+                            else Console.WriteLine("not a valid entry");
+                        }
+
+                        while (true)
+                        {
+
+                            Console.WriteLine("What species of pet is it");
+                            Console.WriteLine("Dog, Cat, Hampster, Lizard, Bird, Robot");
+                            string petSpecies = Console.ReadLine().ToLower();
+                            myPet.PetSpecies = petSpecies;
+
+                            if (petSpecies == "dog")
+                                break;
+                            else if (petSpecies == "cat")
+                                break;
+                            else if (petSpecies == "hampster")
+                                break;
+                            else if (petSpecies == "lizard")
+                                break;
+                            else if (petSpecies == "bird")
+                                break;
+                            else if (petSpecies == "robot")
+                                break;
+                        
+                            else Console.WriteLine("not a valid entry");
+
+                        }
+                         Console.Clear();
+                        
+                        
                         break;
+
+                        
                       
                     case ("info"):
                         
                         Console.Clear();
-                        Console.WriteLine("Your pet name is " + myPet.PetName + " and it is " + myPet.PetType);
-                       
+                        Console.WriteLine("Your pet name is " + myPet.PetName + " and it is an " + myPet.PetType + " " + myPet.PetSpecies);
                         break;
 
+                    case ("status"):
+                        Console.Clear();
+                        Console.WriteLine("Your pets status is:");
+                        Console.WriteLine("hunger " + myPet.PetHunger);
+                        Console.WriteLine("boredom " + myPet.PetBoredom);
+                        Console.WriteLine("health " + myPet.PetHealth);
+                        break;
+
+                    case ("feed"):
+                        Console.Clear();
+                        Console.WriteLine("You have fed your pet.");
+                        myPet.PetHunger -- ;
+                        break;
                     default:
                         Console.WriteLine("Please enter proper response");
                         break;
